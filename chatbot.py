c66a0ba7fe65ef79574c370a8e300b9990f13e6e
Michael Taylor <90michaeltaylor@gmail.com>
2023-08-19 19:30:43 -0400

    Poetry and Pydantic.

--- chatbot.py
+++ chatbot.py
@@ -1,23 +1,44 @@
-import openai
-import discord
+import asyncio
 import logging
 from collections import defaultdict, deque
-import asyncio
 from datetime import datetime, timedelta
-import json
-import os
+from typing import List
+
+import discord
+import openai
+from pydantic import FilePath
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    discord_bot_token: str
+    openai_api_key: str
+    max_cache: int = 5
+    cooldown_time: int = 2
+    openai_model: str = "gpt-3.5-turbo-16k"
+    allowed_channel_ids: List[int] = []
+    system_message_file: FilePath = FilePath("./system_message.txt")
+
+    @property
+    def system_message(self):
+        with open(self.system_message_file, "r", encoding="utf-8") as file:
+            return file.read()
+
+    model_config = SettingsConfigDict(
+        # `.env.prod` takes priority over `.env`
+        env_file=(".env", ".env.prod"),
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+
+settings = Settings()
 
 # Logging setup
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
-
-# Load configurations from environment variables
-TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-MAX_CACHE = int(os.environ.get('MAX_CACHE', 5))
-COOLDOWN_TIME = int(os.environ.get('COOLDOWN_TIME', 2))
-ROLE_ID = int(os.environ.get('ROLE_ID'))
-OPENAI_MODEL = os.environ.get('OPENAI_MODEL', 'gpt-3.5-turbo-16k')
 
 # Data structures for caching, cooldown, and last interaction
 user_message_cache = defaultdict(deque)
@@ -35,26 +56,30 @@ intents.messages = True  # Required for message.reference
 bot = discord.Client(intents=intents)  # Using the Client class
 
 
-
 # Set up OpenAI API
-openai.api_key = OPENAI_API_KEY
+openai.api_key = settings.openai_api_key
+
 
 def split_message(content, limit=2000):
     """Split a message into chunks of a specified limit."""
-    return [content[i:i+limit] for i in range(0, len(content), limit)]
+    return [content[i : i + limit] for i in range(0, len(content), limit)]
+
 
 async def send_large_message(channel, content):
     """Send content which may be longer than the discord character limit by splitting it into parts."""
     for chunk in split_message(content):
         await channel.send(chunk)
 
+
 # Load the system message from file
-with open('system_message.txt', 'r', encoding='utf-8') as file:
+with open("system_message_copy.txt", "r", encoding="utf-8") as file:
     SYSTEM_MESSAGE = file.read()
+
 
 @bot.event
 async def on_ready():
-    logger.info(f'We have logged in as {bot.user.name}')
+    logger.info(f"We have logged in as {bot.user.name}")
+
 
 @bot.event
 async def on_message(message):
@@ -62,32 +87,35 @@ async def on_message(message):
         return
 
     mentioned = bot.user in message.mentions
-    replied_to_bot = message.reference and message.reference.resolved.author.id == bot.user.id
+    replied_to_bot = (
+        message.reference and message.reference.resolved.author.id == bot.user.id
+    )
     last_interaction = user_last_interaction[message.author.id]
-    within_timeframe = datetime.utcnow() - last_interaction["time"] <= timedelta(seconds=120)
+    within_timeframe = datetime.utcnow() - last_interaction["time"] <= timedelta(
+        seconds=120
+    )
     same_channel = last_interaction["channel"] == message.channel.id
 
     if not (mentioned or replied_to_bot or (within_timeframe and same_channel)):
         return
 
-    logger.info(f"Initiating conversation with user: {message.author.name} (ID: {message.author.id})")
+    logger.info(
+        f"Initiating conversation with user: {message.author.name} (ID: {message.author.id})"
+    )
 
-    role = message.guild.get_role(ROLE_ID)
-    if not role:
-        logging.error(f"Role with ID {ROLE_ID} (type: {type(ROLE_ID)}) not found!")
-        return
+    async with message.channel.typing():
+        await chat_with_openai(message)
+        user_last_interaction[message.author.id] = {
+            "time": datetime.utcnow(),
+            "channel": message.channel.id,
+        }
 
-    if role not in message.author.roles and not any(r >= role for r in message.author.roles):
-        logger.warning(f"Unauthorized access attempt by user: {message.author.name} (ID: {message.author.id})")
-        await message.channel.send("Sorry, you don't have the permission to interact with me!")
-        return
-
-    await chat_with_openai(message)
-    user_last_interaction[message.author.id] = {"time": datetime.utcnow(), "channel": message.channel.id}
 
 async def chat_with_openai(message):
     if user_cooldown[message.author.id]:
-        logger.info(f"Cooldown enforced for user: {message.author.name} (ID: {message.author.id})")
+        logger.info(
+            f"Cooldown enforced for user: {message.author.name} (ID: {message.author.id})"
+        )
         await message.channel.send("Please wait a bit before chatting again!")
         return
 
@@ -98,9 +126,7 @@ async def chat_with_openai(message):
 
     typing_task = asyncio.create_task(keep_typing(message.channel))
 
-    messages = [
-        {"role": "system", "content": SYSTEM_MESSAGE}
-    ]
+    messages = [{"role": "system", "content": settings.system_message}]
 
     for role, msg_content in user_message_cache[message.author.id]:
         messages.append({"role": role, "content": msg_content})
@@ -109,41 +135,53 @@ async def chat_with_openai(message):
 
     try:
         response = openai.ChatCompletion.create(
-            model=OPENAI_MODEL,
-            messages=messages
+            model=settings.openai_model, messages=messages
         )
 
         typing_task.cancel()
-        ai_message = response['choices'][0]['message']['content']
+        ai_message = response["choices"][0]["message"]["content"]
         await send_large_message(message.channel, ai_message)
 
-        if len(user_message_cache[message.author.id]) >= MAX_CACHE:
+        if len(user_message_cache[message.author.id]) >= settings.max_cache:
             removed_message = user_message_cache[message.author.id].popleft()
-            logger.info(f"Removed oldest cached message for user: {message.author.name} (ID: {message.author.id}). Message: {removed_message[1]}")
+            logger.info(
+                f"Removed oldest cached message for user: {message.author.name} (ID: {message.author.id}). Message: {removed_message[1]}"
+            )
 
         user_message_cache[message.author.id].append(("user", message.content))
-        logger.info(f"Added message to cache for user: {message.author.name} (ID: {message.author.id}). Message: {message.content}")
+        logger.info(
+            f"Added message to cache for user: {message.author.name} (ID: {message.author.id}). Message: {message.content}"
+        )
 
         user_message_cache[message.author.id].append(("assistant", ai_message))
-        logger.info(f"Added bot response to cache for user: {message.author.name} (ID: {message.author.id}). Response: {ai_message}")
+        logger.info(
+            f"Added bot response to cache for user: {message.author.name} (ID: {message.author.id}). Response: {ai_message}"
+        )
 
-        user_cooldown[message.author.id] = COOLDOWN_TIME
+        user_cooldown[message.author.id] = settings.cooldown_time
         bot.loop.create_task(cooldown_user(message.author.id))
-        logger.info(f"Set cooldown for user: {message.author.name} (ID: {message.author.id})")
+        logger.info(
+            f"Set cooldown for user: {message.author.name} (ID: {message.author.id})"
+        )
 
     except openai.error.OpenAIError as e:
         typing_task.cancel()
         await message.channel.send(f"An error occurred: {str(e)}")
-        logger.error(f"OpenAI API error for user: {message.author.name} (ID: {message.author.id}). Error: {str(e)}")
+        logger.error(
+            f"OpenAI API error for user: {message.author.name} (ID: {message.author.id}). Error: {str(e)}"
+        )
     except Exception as e:
         typing_task.cancel()
-        logger.error(f"Unexpected error for user: {message.author.name} (ID: {message.author.id}). Error: {e}")
+        logger.error(
+            f"Unexpected error for user: {message.author.name} (ID: {message.author.id}). Error: {e}"
+        )
+
 
 async def cooldown_user(user_id):
     logger.info(f"Starting cooldown for user ID: {user_id}")
-    await asyncio.sleep(COOLDOWN_TIME)
+    await asyncio.sleep(settings.cooldown_time)
     user_cooldown[user_id] = 0
     logger.info(f"Cooldown ended for user ID: {user_id}")
 
-bot.run(TOKEN)
 
+bot.run(settings.discord_bot_token)
